3e7b2065e5e76bff54bd057f1c7e9da489ed6541
jordan <jordan@www.jlaqui.com>
2022-05-24 19:18:01 -0500

    Avance maps

--- app/src/main/java/com/ges/easyparking/screens/HomeScreen.kt
+++ app/src/main/java/com/ges/easyparking/screens/HomeScreen.kt
@@ -1,15 +1,29 @@
 package com.ges.easyparking.screens
 
+import android.icu.text.Transliterator
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import com.ges.easyparking.components.BottomBarFilter
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
 
 @Composable
 fun HomeScreen(navController: NavController){
     Scaffold {
-        BottomBarFilter()
-
+        MyMap()
+        //BottomBarFilter()
     }
 }
 
+@Composable
+fun MyMap() {
+    val marker = LatLng(-16.411989090301443, -71.51048341048909)
+    GoogleMap(modifier = Modifier.fillMaxSize(), properties = ) {
+        Marker(position = marker)
+
+    }
+
+}
