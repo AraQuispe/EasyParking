a9fdb18a8409b1275afb70111ed506a5ae8d73b9
Araceli Quispe <maraceliquispey@gmail.com>
2022-06-22 09:59:49 -0500

    Data update

--- app/src/main/java/com/ges/easyparking/screens/home/map/HomeScreen.kt
+++ app/src/main/java/com/ges/easyparking/screens/home/map/HomeScreen.kt
@@ -27,6 +27,10 @@ fun HomeScreen(navController: NavController){
 @Composable
 fun MyGoogleMaps(){
     val marker = LatLng(-16.3991588, -71.5314951)
+    val marker1 = LatLng(-16.4154919, -71.5615347)
+    val marker2 = LatLng(-16.417815, -71.5605144)
+    val marker3 = LatLng(-16.4145078, -71.5597471)
+
     val cameraPosition = rememberCameraPositionState{
         position = CameraPosition.fromLatLngZoom(marker, 10f)
     }
@@ -34,8 +38,23 @@ fun MyGoogleMaps(){
         GoogleMap(cameraPositionState = cameraPosition){
             Marker(
                 position=marker,
-                title = "Bagua",
-                snippet = "Mark"
+                title = "Park Center",
+                snippet = "Pierola 201"
+            )
+            Marker(
+                position=marker1,
+                title = "Terrapuerto Parqueo Exterior",
+                snippet = "Arequipa 0401"
+            )
+            Marker(
+                position=marker2,
+                title = "S.A PARKING",
+                snippet = "C. San Juan de Dios 611"
+            )
+            Marker(
+                position=marker3,
+                title = "Estacionamientos Parque Lambramani",
+                snippet = " Estación Lambramani, Arequipa 04002"
             )
         }
     }
